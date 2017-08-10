d83d45b12b7340b1309195c81149cc71d5c86665
Erdem OZDEMIR <erdem.ozdemir@hepsiburada.com>
2017-08-10 16:26:46 +0300

    Add project files.

---
+++ XUnitTestProject1/RomanNumberConverter.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+
+namespace XUnitTestProject1
+{
+    public class RomanNumberConverter
+    {
+        private readonly Dictionary<int, string> arabicToRomanDictionary = new Dictionary<int, string>();
+
+        public RomanNumberConverter()
+        {
+            this.arabicToRomanDictionary.Add(1, "I");
+            this.arabicToRomanDictionary.Add(5, "V");
+            this.arabicToRomanDictionary.Add(10, "X");
+            this.arabicToRomanDictionary.Add(50, "L");
+            this.arabicToRomanDictionary.Add(100, "C");
+            this.arabicToRomanDictionary.Add(500, "D");
+            this.arabicToRomanDictionary.Add(1000, "M");
+        }
+        public string ConvertToRomanNumber(int input)
+        {
+            if (this.arabicToRomanDictionary.ContainsKey(input)) { return this.arabicToRomanDictionary[input]; }
+
+            if (input == 4) return "IV";
+
+            return string.Empty;
+        }
+    }
+}
---
+++ XUnitTestProject1/UnitTest1.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using FluentAssertions;
+
+using Xunit;
+
+namespace XUnitTestProject1
+{
+    public class UnitTest1
+    {
+        [Theory]
+        [ClassData(typeof(PrimitiveInput))]
+        public void given_primitive_numbers_for_roman_when_converting_then_success(int input, string expected)
+        {
+            RomanNumberConverter sut = new RomanNumberConverter();
+            string actual = sut.ConvertToRomanNumber(input);
+            actual.Should().Be(expected);
+        }
+        [Theory]
+        [ClassData(typeof(SubtructedInput))]
+        public void given_subtructed_numbers_for_roman_when_converting_then_success(int input, string expected)
+        {
+            RomanNumberConverter sut = new RomanNumberConverter();
+            string actual = sut.ConvertToRomanNumber(input);
+            actual.Should().Be(expected);
+        }
+    }
+    internal class PrimitiveInput : IEnumerable<object[]>
+    {
+        IList<object[]> data = new List<object[]>{
+                                                     new object[]{1,"I"},
+                                                     new object[]{5,"V"},
+                                                     new object[]{10,"X"},
+                                                     new object[]{50,"L"},
+                                                     new object[]{100,"C"},
+                                                     new object[]{500,"D"},
+                                                     new object[]{1000,"M"},
+                                                 };
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            return this.data.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+    internal class SubtructedInput : IEnumerable<object[]>
+    {
+        IList<object[]> data = new List<object[]>{
+                                                     new object[]{4,"IV"},
+                                                     // new object[]{9,"IX"},
+                                                     // new object[]{40,"XL"},
+                                                     // new object[]{90,"LC"},
+                                                     // new object[]{400,"CD"},
+                                                     // new object[]{900,"DM"},
+                                                 };
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            return this.data.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
