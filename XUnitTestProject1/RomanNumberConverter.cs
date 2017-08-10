8bd4bb0d59468dab8dbf2691794650be70fc8798
erdem <nerobianchi@gmail.com>
2017-08-11 01:09:46 +0300

    4 times repeated romans are refactored gracefully

--- XUnitTestProject1/RomanNumberConverter.cs
+++ XUnitTestProject1/RomanNumberConverter.cs
@@ -28,52 +28,41 @@ namespace XUnitTestProject1
                 var divisor = this.FindMaxDivisor(number);
                 number -= divisor.Key;
                 romanNumber = romanNumber + divisor.Value;
-
-               romanNumber= this.Normalize(romanNumber);
             }
+            romanNumber = this.Normalize(romanNumber);
 
-            
-//            foreach (var pair in this.arabicToRomanDictionary)
-//            {
-//                if (number < pair.Key)
-//                {
-//                    if (number == 5 - 1) return "I" + "V";
-//                    if (number == 10 - 1) return "I" + "X";
-//                    if (number == 50 - 10) return "X" + "L";
-//                    if (number == 100 - 10) return "X" + "C";
-//                }
-//            }
-//            if (number < 10)
-//            {
-//                if (number == 5 - 1) return "I" + "V";
-//                if (number == 10 - 1) return "I" + "X";
-//            }
-//            if (number < 100)
-//            {
-//                if (number == 40) return "X" + "L";
-//                if (number == 90) return "X" + "C";
-//            }
             return romanNumber;
         }
 
         private string Normalize(string romanNumber)
         {
-            return romanNumber
-                              .Replace("DCCCC", "CM")
-                              .Replace("CCCC", "CD")
-                              .Replace("LXXXX", "XC")
-                              .Replace("XXXX", "XL")
-                              .Replace("VIIII", "IX")
-                              .Replace("IIII", "IV")
-                
-                ;
+//            return romanNumber
+//                              .Replace("DCCCC", "CM")
+//                              .Replace("CCCC", "CD")
+//                              .Replace("LXXXX", "XC")
+//                              .Replace("XXXX", "XL")
+//                              .Replace("VIIII", "IX")
+//                              .Replace("IIII", "IV")
+//                ;
+//            
+            List<string> list = new List<string>(this.arabicToRomanDictionary.Values);
+            for (int i = 1; i < list.Count; i = i + 2)
+            {
+                var high = list[i];
+                var low = list[i + 1];
+                var repeated = "".PadRight(4, low.ToCharArray()[0]);
+                var highLord = list[i - 1];
+                romanNumber = romanNumber.Replace(high + repeated, low + highLord);
+                romanNumber = romanNumber.Replace(repeated, low + high);
+            }
+            return romanNumber;
         }
 
         private KeyValuePair<int, string> FindMaxDivisor(int number)
         {
             foreach (var pair in this.arabicToRomanDictionary)
             {
-                if (number/pair.Key >= 1)
+                if (number / pair.Key >= 1)
                 {
                     return pair;
                 }
