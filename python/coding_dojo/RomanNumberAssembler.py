a768ec68bb131b029243c133bf73401c2752027b
nerobianchi <nerobianchi@gmail.com>
2017-08-15 01:23:49 +0300

    subtracted ones are refactored

--- python/coding_dojo/RomanNumberAssembler.py
+++ python/coding_dojo/RomanNumberAssembler.py
@@ -14,16 +14,19 @@ class RomanNumberAssembler:
         if roman_mapping is not None:
             return roman_mapping
 
-        if number >= 100:
-            return self.get_subtracted(number, 500, 100, 1000, "D", "C", "M")
-        if number >= 10:
-            return self.get_subtracted(number, 50, 10, 100, "L", "X", "C")
-        if number >= 1:
-            return self.get_subtracted(number, 5, 1, 10, "V", "I", "X")
+        return self.try_subtracted(number)
 
     def get_subtracted(self, number, high, low, high_lord, high_roman, low_roman, high_lord_roman):
         if number == high - low:
             return low_roman + high_roman
         if number == high_lord - low:
             return low_roman + high_lord_roman
+
+    def try_subtracted(self, number):
+        if number >= 100:
+            return self.get_subtracted(number, 500, 100, 1000, "D", "C", "M")
+        if number >= 10:
+            return self.get_subtracted(number, 50, 10, 100, "L", "X", "C")
+        if number >= 1:
+            return self.get_subtracted(number, 5, 1, 10, "V", "I", "X")
 
